80fa18d1e6be02451b66511d8c03179c38bdc6ae
Mark Houghton <mark1284@gmail.com>
2012-05-22 10:19:57 +0100

    Initial Commit

---
+++ XcodeGitTestTests/XcodeGitTestTests.h
@@ -0,0 +1,13 @@
+//
+//  XcodeGitTestTests.h
+//  XcodeGitTestTests
+//
+//  Created by Mark Houghton on 22/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface XcodeGitTestTests : SenTestCase
+
+@end
---
+++ XcodeGitTestTests/XcodeGitTestTests.m
@@ -0,0 +1,32 @@
+//
+//  XcodeGitTestTests.m
+//  XcodeGitTestTests
+//
+//  Created by Mark Houghton on 22/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "XcodeGitTestTests.h"
+
+@implementation XcodeGitTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in XcodeGitTestTests");
+}
+
+@end
