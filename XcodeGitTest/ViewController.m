531257c98bc8fdbb0e814e0337fb1b6c819c07bb
Mark Houghton <mark1284@gmail.com>
2012-05-22 10:20:53 +0100

    initial commit

--- XcodeGitTest/ViewController.m
+++ XcodeGitTest/ViewController.m
@@ -24,6 +24,7 @@
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+    // some stuff
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
